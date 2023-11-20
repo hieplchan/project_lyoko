e4d773d69bd5a81e41f526e980485606c4082490
Hiep Chan <hiep.lchan@gmail.com>
2023-11-20 21:50:25 +0700

    modify some slope move control

--- Assets/Scripts/Characters/CharacterMoveController.cs
+++ Assets/Scripts/Characters/CharacterMoveController.cs
@@ -7,28 +7,41 @@ public class CharacterMoveController : MonoBehaviour
 {
     [SerializeField, Range(0f, 100f)] float maxSpeed = 10f;
     [SerializeField, Range(0f, 100f)] float maxAccelerate = 10f;
-    [SerializeField, Range(0f, 100f)] float maxAirAccelerate = 1f; // hard to control when inair
-    [SerializeField, Range(0f, 10f)] float jumpHeight = 2f;
+    [Tooltip("Hard to control when in air")]
+    [SerializeField, Range(0f, 100f)] float maxAirAccelerate = 1f;
+    [Tooltip("Dowble jump, tripble jump...")]
     [SerializeField, Range(0, 5)] int maxAirJumps = 2;
+    [Tooltip("How high character can do in single jump")]
+    [SerializeField, Range(0f, 10f)] float jumpHeight = 2f;
+    [SerializeField, Range(0f, 90f)] float maxGroundAngle = 25f;
 
     private Rigidbody _body;
 
     private Vector2 _playerInput;
     private Vector3 _desiredVelocity;
     private bool _desiredJump;
+    private float _minGroundDotProduct;
 
     private float _currentAccelerate;
     private Vector3 _velocity;
     private float _maxSpeedChange;
     private int _currentAirJump;
     private float _jumpSpeed;
+    // While in air/double jump, not have contact normal => use Vector3.up
+    private Vector3 _currentContactNormal;
 
     public bool OnGround => _onGround;
     private bool _onGround;
 
+    private void OnValidate()
+    {
+        _minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+    }
+
     private void Awake()
     {
         _body = GetComponent<Rigidbody>();
+        OnValidate();
     }
 
     private void OnCollisionEnter(Collision collision) => EvaluateCollision(collision);
@@ -72,9 +85,19 @@ public class CharacterMoveController : MonoBehaviour
     private void UpdateState()
     {
         _velocity = _body.velocity;
-        if (_onGround) _currentAirJump = 0;
+        if (_onGround)
+        {
+            _currentAirJump = 0;
+        }
+        else
+        {            
+            _currentContactNormal = Vector3.up;
+        }
     }
 
+    // https://catlikecoding.com/unity/tutorials/movement/physics/#2.2
+    // https://catlikecoding.com/unity/tutorials/movement/physics/#2.6
+    // https://catlikecoding.com/unity/tutorials/movement/physics/#3.4
     private void Jump()
     {
         if (_onGround || _currentAirJump < maxAirJumps)
@@ -82,20 +105,26 @@ public class CharacterMoveController : MonoBehaviour
             _currentAirJump++;
             _jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
 
-            // limit upward velocity
-            if (_velocity.y > 0f)
-                _jumpSpeed = Mathf.Max(_jumpSpeed - _velocity.y, 0f);
+            // limit jump initial velocity
+            float alignSpeed = Vector3.Dot(_velocity, _currentContactNormal);
+            if (alignSpeed > 0f)
+                _jumpSpeed = Mathf.Max(_jumpSpeed - alignSpeed, 0f);
 
-            _velocity.y += _jumpSpeed;
+            _velocity += _currentContactNormal * _jumpSpeed;
         }
     }
 
+    // https://catlikecoding.com/unity/tutorials/movement/physics/#3.3
     private void EvaluateCollision(Collision collision)
     {
         for (int i = 0; i < collision.contactCount; i++)
         {
             Vector3 normal = collision.GetContact(i).normal;
-            _onGround |= normal.y >= 0.9f;
+            if (normal.y >= _minGroundDotProduct)
+            {
+                _onGround = true;
+                _currentContactNormal = normal;
+            }
         }
     }
 }
