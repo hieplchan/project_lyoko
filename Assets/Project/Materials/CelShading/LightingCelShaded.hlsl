6d41f45bad1a62655c3cc33f10b4c321b95832ef
StartledSeal <hiep.lchan@gmail.com>
2024-02-06 16:23:47 +0700

    add cel shading but fail

--- Assets/Project/Materials/CelShading/LightingCelShaded.hlsl
+++ Assets/Project/Materials/CelShading/LightingCelShaded.hlsl
@@ -4,6 +4,17 @@
 #ifndef LIGHTING_CEL_SHADED_INCLUDED
 #define LIGHTING_CEL_SHADED_INCLUDED
 
+struct EdgeConstances
+{
+    float diffuse;
+    float specular;
+    float specularOffset;
+    float distanceAttenuation;
+    float shadowAttenuation;
+    float rim;
+    float rimOffset;
+};
+
 struct SurfaceVariables
 {
     float smoothness;
@@ -12,12 +23,16 @@ struct SurfaceVariables
     float3 view;
 
     float shininess;
+
+    EdgeConstances ec;
 };
 
 float3 CalculateCelShading(Light l, SurfaceVariables s)
 {
     // Shadow
-    float attenuation = l.shadowAttenuation * l.distanceAttenuation;
+    float shadowAttenuationSmoothStepped = smoothstep(0.0f, s.ec.shadowAttenuation, l.shadowAttenuation);
+    float distanceAttenuationSmoothStepped = smoothstep(0.0f, s.ec.distanceAttenuation, l.distanceAttenuation);
+    float attenuation = shadowAttenuationSmoothStepped * distanceAttenuationSmoothStepped;
     
     // Diffuse reflection = dot(normal, dir_from_point_to_light)
     // https://learnwebgl.brown37.net/09_lights/lights_diffuse.html
@@ -35,12 +50,28 @@ float3 CalculateCelShading(Light l, SurfaceVariables s)
     // https://www.roxlu.com/2014/037/opengl-rim-shader
     float rim = 1 - dot(s.view, s.normal);
     rim *= pow(diffuse, s.rimThreshold);
+
+    // Cel Shading
+    diffuse = smoothstep(0.0f, s.ec.diffuse, diffuse);
+    specular = s.smoothness * smoothstep(
+        (1 - s.smoothness) * s.ec.specular + s.ec.specularOffset,
+        s.ec.specular + s.ec.specularOffset,
+        specular
+    );
+    rim = s.smoothness * smoothstep(
+        s.ec.rim - 0.5f * s.ec.rimOffset,
+        s.ec.rim + 0.5f * s.ec.rimOffset,
+        rim
+    );
     
     return l.color * (diffuse + max(rim, specular));
 }
 
 void LightingCelShaded_float(
     float3 Smoothness, float RimThreshold,
+    float EdgeDiffuse, float EdgeSpecular, float EdgeSpecularOffset,
+    float EdgeDistanceAttenuation, float EdgeShadowAttenuation,
+    float EdgeRim, float EdgeRimOffset,
     float3 Position, float3 Normal, float3 View,
     out float3 Color)
 {
@@ -50,6 +81,14 @@ void LightingCelShaded_float(
     s.smoothness = Smoothness;
     s.shininess = exp2(10 * Smoothness + 1);
     s.rimThreshold = RimThreshold;
+    
+    s.ec.diffuse = EdgeDiffuse;
+    s.ec.specular = EdgeSpecular;
+    s.ec.specularOffset = EdgeSpecularOffset;
+    s.ec.distanceAttenuation = EdgeDistanceAttenuation;
+    s.ec.shadowAttenuation = EdgeShadowAttenuation;
+    s.ec.rim = EdgeRim;
+    s.ec.rimOffset = EdgeRimOffset;
     
     #if SHADOWS_SCREEN
     float4 clipPos = TransformWorldToHClip(Position);
