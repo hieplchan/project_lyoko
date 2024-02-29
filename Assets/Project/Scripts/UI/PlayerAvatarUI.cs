36ae0bd923fb4eede47bf97956b5d58580aaca2c
StartledSeal <hiep.lchan@gmail.com>
2024-02-29 15:18:31 +0700

    add heart beat anim to avatar

--- Assets/Project/Scripts/UI/PlayerAvatarUI.cs
+++ Assets/Project/Scripts/UI/PlayerAvatarUI.cs
@@ -1,15 +1,24 @@
+using BrunoMikoski.AnimationSequencer;
+using KBCore.Refs;
+using Sirenix.OdinInspector;
 using SuperMaxim.Messaging;
 using UnityEngine;
 
 namespace StartledSeal
 {
-    public class PlayerAvatarUI : MonoBehaviour
+    public class PlayerAvatarUI : ValidatedMonoBehaviour
     {
         [SerializeField] private UIOutline _outline;
+
+        [Header("Heart Beat Effect")] 
+        [SerializeField] private float _minAnimSpeed = 0.6f;
+        [SerializeField] private float _maxAnimSpeed = 1.6f;
+        [SerializeField, Child] private AnimationSequencerController _heartbeatAnimController;
 
         private void Awake()
         {
             Messenger.Default.Subscribe<PlayerDeadEventPayload>(HandlePlayerDeadEvent);
+            SetHeartbeatSpeed(_minAnimSpeed);
         }
 
         private void OnDestroy()
@@ -20,6 +29,13 @@ namespace StartledSeal
         private void HandlePlayerDeadEvent(PlayerDeadEventPayload payload)
         {
             _outline.color = Color.red;
+            SetHeartbeatSpeed(0f);
+        }
+
+        [Button]
+        private void SetHeartbeatSpeed(float timeScale)
+        {
+            _heartbeatAnimController.PlayingSequence.timeScale = timeScale;
         }
     }
 }
