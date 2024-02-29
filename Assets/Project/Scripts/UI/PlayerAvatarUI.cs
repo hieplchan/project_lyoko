da66e5e340bf396a628e4f9f12b93495f0d86c6d
StartledSeal <hiep.lchan@gmail.com>
2024-02-29 17:27:05 +0700

    add heartbeat color avatar

--- Assets/Project/Scripts/UI/PlayerAvatarUI.cs
+++ Assets/Project/Scripts/UI/PlayerAvatarUI.cs
@@ -10,9 +10,11 @@ namespace StartledSeal
 {
     public class PlayerAvatarUI : ValidatedMonoBehaviour
     {
+        
         [SerializeField] private UIOutline _outline;
 
         [Header("Heart Beat Effect")] 
+        [SerializeField] private Gradient _outlineBGGradient;
         [SerializeField] private float _minAnimSpeed = 0.6f;
         [SerializeField] private float _maxAnimSpeed = 1.6f;
         [SerializeField, Child] private AnimationSequencerController _heartbeatAnimController;
@@ -22,6 +24,8 @@ namespace StartledSeal
             Messenger.Default.Subscribe<PlayerDeadEventPayload>(HandlePlayerDeadEvent);
             Messenger.Default.Subscribe<NearbyEnemyDistancePayload>(HandleNearbyEnemyDistanceMessage);
             SetHeartbeatSpeed(_minAnimSpeed);
+
+            _outline.color = _outlineBGGradient.Evaluate(0f);
         }
 
         private void OnDestroy()
@@ -32,12 +36,15 @@ namespace StartledSeal
         
         private void HandlePlayerDeadEvent(PlayerDeadEventPayload payload)
         {
-            _outline.color = Color.red;
+            Messenger.Default.Unsubscribe<NearbyEnemyDistancePayload>(HandleNearbyEnemyDistanceMessage);
+
+            _outline.color = _outlineBGGradient.Evaluate(1f);
             SetHeartbeatSpeed(0f);
         }
         
         private void HandleNearbyEnemyDistanceMessage(NearbyEnemyDistancePayload payload)
         {
+            _outline.color = _outlineBGGradient.Evaluate(payload.WarningRatio);
             SetHeartbeatSpeed(Mathf.Lerp(_minAnimSpeed, _maxAnimSpeed, payload.WarningRatio));
         }
 
