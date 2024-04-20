3d6c7a39f373544816e5d2d8de8f33edf20a40d4
StartledSeal <hiep.lchan@gmail.com>
2024-04-20 13:49:45 +0700

    polish some animation

--- Assets/Scripts/Ingame/Player/GroundChecker.cs
+++ Assets/Scripts/Ingame/Player/GroundChecker.cs
@@ -7,7 +7,8 @@ namespace StartledSeal
 {
     public class GroundChecker : ValidatedMonoBehaviour
     {
-        private const float HeightOffset = 0.1f;
+        [SerializeField] private bool isGizmoEnabled;
+        [SerializeField] private float HeightOffset = 0.1f;
 
         [SerializeField, Self] private CapsuleCollider _capsuleCollider;
         [SerializeField] private LayerMask _groundLayers;
@@ -29,15 +30,17 @@ namespace StartledSeal
                 out _, HeightOffset * 2, _groundLayers);
         }
 
-        // private void OnDrawGizmosSelected() 
-        // {
-        //     // Start point
-        //     Gizmos.color = Color.yellow;
-        //     Gizmos.DrawWireSphere(transform.position + new Vector3(0f, _groundDistance + HeightOffset, 0f), _groundDistance);
-        //     
-        //     // End point
-        //     Gizmos.color = Color.red;
-        //     Gizmos.DrawWireSphere(transform.position + new Vector3(0f, _groundDistance + HeightOffset - HeightOffset *2, 0f), _groundDistance);
-        // }
+        private void OnDrawGizmosSelected()
+        {
+            if (!isGizmoEnabled) return;
+            
+            // Start point
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position + new Vector3(0f, _groundDistance + HeightOffset, 0f), _groundDistance);
+            
+            // End point
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position + new Vector3(0f, _groundDistance + HeightOffset - HeightOffset *2, 0f), _groundDistance);
+        }
     }
 }
--- Assets/Scripts/Ingame/Player/States/AttackState.cs
+++ Assets/Scripts/Ingame/Player/States/AttackState.cs
@@ -13,8 +13,8 @@ namespace StartledSeal
 
         public override void OnEnter()
         {
-            // _animator.CrossFade(AttackHash, CrossFadeDuration);
-            _animator.Play(AttackHash, 0, 0f);
+            _animator.CrossFade(AttackHash, 0.001f);
+            // _animator.Play(AttackHash, 0, 0f);
             _player.Attack();
             _player.SetStateHash(AttackHash);
         }
