55680d3d2bc14e515f864fb56d41702bf6e7d796
Hiep Chan <hiep.lchan@gmail.com>
2024-05-07 15:10:20 +0700

    add cuttable grass

--- Assets/Scripts/Ingame/Mechanics/CuttableGrass.cs
+++ Assets/Scripts/Ingame/Mechanics/CuttableGrass.cs
@@ -1,26 +1,18 @@
-using System;
-using System.Threading.Tasks;
-using BrunoMikoski.AnimationSequencer;
 using Cysharp.Threading.Tasks;
-using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace StartledSeal
 {
-    public class BreakableObject: MonoBehaviour, IDamageable
+    public class CuttableGrass : MonoBehaviour, IDamageable
     {
         [SerializeField] private ParticleSystem _vfx;
         [SerializeField] private MeshRenderer _meshRenderer;
-        [SerializeField] private Collider _collider;
         
-        [Button]
         public async UniTask TakeDamage(int damageAmount)
         {
             if (_vfx != null && !_vfx.isPlaying)
                 _vfx!.Play();
-            _meshRenderer!.enabled = false;
-            _collider!.enabled = false;
+            _meshRenderer.enabled = false;
 
             if (_vfx != null)
                 await UniTask.WaitUntil(() => _vfx.isStopped);
