fd572e10adbec991801440a566746aa6867655c2
Hiep Chan <hiep.lchan@gmail.com>
2024-05-10 17:06:03 +0700

    add shotgun vfx

--- Assets/Scripts/Ingame/Player/Equipment/Shotgun.cs
+++ Assets/Scripts/Ingame/Player/Equipment/Shotgun.cs
@@ -1,7 +1,56 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
 namespace StartledSeal
 {
     public class Shotgun : BaseEquipment
     {
+        [SerializeField] private float _attackDistance = 1.8f;
+        [SerializeField] private float _attackAngle = 180f;
+        [SerializeField] private int _attackDamage = 10;
+
+        [SerializeField] private bool _isShowGizmos;
+
         public override bool IsUsable() => true;
+        
+        public override async UniTask Use(Animator _animatorComp)
+        {
+            base.Use(_animatorComp);
+            
+            var _originTransform = _weaponController.gameObject.transform;
+            
+            var pos = _originTransform.position + Vector3.forward;
+            var hits = Physics.OverlapSphere(pos, _attackDistance);
+            foreach (var hit in hits)
+            {
+                var damageableObj = hit.gameObject.GetComponent<IDamageable>();
+                // Check if hit in attack cone
+                if (damageableObj != null)
+                {
+                    var directionToPlayer = hit.gameObject.transform.position - _originTransform.position;
+                    var angleToPlayer = Vector3.Angle(_originTransform.forward, directionToPlayer);
+                    
+                    // MLog.Debug("PlayerWeaponController", $"angleToPlayer {angleToPlayer}");
+                    
+                    if (Mathf.Abs(angleToPlayer) < _attackAngle / 2)
+                        damageableObj.TakeDamage(_attackDamage);
+                }
+            }
+        }
+        
+        private void OnDrawGizmos()
+        {
+            if (!_isShowGizmos) return;
+            
+            var _originTransform = _weaponController.gameObject.transform;
+
+            // Draw attack cone
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(_originTransform.position, _attackDistance);
+            Vector3 forwardConeDirection = Quaternion.Euler(0, _attackAngle / 2, 0) * _originTransform.forward * _attackDistance;
+            Vector3 backwardConeDirection = Quaternion.Euler(0, -_attackAngle / 2, 0) * _originTransform.forward * _attackDistance;
+            Gizmos.DrawLine(_originTransform.position, _originTransform.position + forwardConeDirection);
+            Gizmos.DrawLine(_originTransform.position, _originTransform.position + backwardConeDirection);
+        }
     }
 }
